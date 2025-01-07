eba2a4e7f5889a77596f7be55d2f460016c9ded7
AvdeevK <marselo1910@gmail.com>
2025-01-08 01:27:16 +0300

    added sample router to project

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+)
+
+func run(r chi.Router) error {
+	return http.ListenAndServe(":8080", r)
+}
+
+func main() {
+	srv := chi.NewRouter()
+	if err := run(srv); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AvdeevK/loyality-service
 
 go 1.22.5
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
